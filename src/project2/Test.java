09642a35287f008e7eb4fb0845a976479a12771f
VDX14 <vanessa.xiong1@mymail.nwtc.edu>
2026-01-29 11:29:15 -0600

    Revert "Temporary change for revert demonstration"
    This reverts commit 4103203a1809546042dc46441d8af6a58be1f8c3.

--- src/project2/Test.java
+++ src/project2/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Making this change and to be reverted afterwards!");
+		System.out.println("Initial commit");
 
 	}
 
