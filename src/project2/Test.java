6c4ba7857986e24d6b1578a167e6c84d2aba623e
VDX14 <vanessa.xiong1@mymail.nwtc.edu>
2026-01-29 11:46:03 -0600

    Added the initial try-catch-finally exception

--- src/project2/Test.java
+++ src/project2/Test.java
@@ -5,8 +5,23 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Initial commit");
-
+		//Initial try-catch block example
+		try {
+			//Declare an integer variable and assign it the value 4
+			int number = 4;
+			//Attempt to divide by zero, which will cause exception
+			int result = number / 0; 
+			
+			System.out.println("Result: " + result);
+		
+		} catch (ArithmeticException e) {
+			//Print this message to console
+			System.out.println("Error: Unable to divide by zero");
+		
+		} finally {
+			System.out.println("The program is finished executing");
+		}
+		
 	}
 
 }
