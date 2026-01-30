2051f2e03b4a9dfa659c511622beb4a99a6fb018
VDX14 <vanessa.xiong1@mymail.nwtc.edu>
2026-01-30 16:36:27 -0600

    Add Log4j logging with file output

--- src/project2/Test.java
+++ src/project2/Test.java
@@ -1,25 +1,38 @@
 package project2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Test {
+	
+	//Create a logger object using log4j
+	final static Logger Log = LogManager.getLogger(Test.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Log in information message to indicate application has started 
+		Log.info("Application has started");
+		
 		//Initial try-catch block example
 		try {
-			//Declare an integer variable and assign it the value 4
-			int number = 4;
-			//Attempt to divide by zero, which will cause exception
-			int result = number / 0; 
+			//Declare an integer variable and assign it the value 6
+			//Placed in try-catch block in case error occurs
+			int number = 6;
 			
-			System.out.println("Result: " + result);
+			//Have variable divided by 2 
+			int result = number / 2; 
+			
+			Log.info("Calculation was successful: " + result);
 		
-		} catch (ArithmeticException e) {
-			//Print this message to console
-			System.out.println("Error: Unable to divide by zero");
+		} catch (Exception e) {
+			//Catch any exceptions in the try-catch block 
+			//Log the error message along with the exception details
+			Log.info("An error has occured", e);
 		
 		} finally {
-			System.out.println("The program is finished executing");
+			//Used here to Log that the application has completed running
+			Log.info("Application is completed");
 		}
 		
 	}
